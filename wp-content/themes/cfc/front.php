4c7ac7887339358d4fc38f766718c9366e371e83
betkapolymorphic <a_albert@ukr.net>
2015-03-06 07:34:54 -0300

    +middle hero

--- wp-content/themes/cfc/front.php
+++ wp-content/themes/cfc/front.php
@@ -9,7 +9,7 @@ get_header(); ?>
 <div id="logoWords" style="">
 	<div id="logoImg">
 		<a href="https://itunes.apple.com/in/app/cfc/id580661730?mt=12" target="_blank">
-			<img  style="width: 15em" src="<?php echo get_template_directory_uri(); ?>/images/composition.png" />
+			<img  style="width: 12em" src="<?php echo get_template_directory_uri(); ?>/images/composition.png" />
 		</a>
 	</div>
     <!--
