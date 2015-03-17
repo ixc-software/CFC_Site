cd3c5fdf8027aeaba9510a7afc64a26291bb9102
betkapolymorphic <a_albert@ukr.net>
2015-03-17 08:28:02 -0300

    +share resp

--- wp-content/themes/cfc/footer.php
+++ wp-content/themes/cfc/footer.php
@@ -68,5 +68,6 @@
 
 <?php wp_footer(); ?>
 
+<script src="<?php echo get_template_directory_uri(); ?>/js/rrssb.min.js"></script>
 </body>
 </html>
--- wp-content/themes/cfc/header.php
+++ wp-content/themes/cfc/header.php
@@ -30,7 +30,7 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/rrssb.css" />
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css">
 
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/hover.css">
