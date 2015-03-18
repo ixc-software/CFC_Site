4dd7b6f74ab1a72d5596a44891b31f02c6ac0149
betkapolymorphic <a_albert@ukr.net>
2015-03-18 09:07:30 -0300

    mouseover

--- wp-content/themes/cfc/js/index.js
+++ wp-content/themes/cfc/js/index.js
@@ -1,16 +1,26 @@
 /**
  * Created by Alexeev on 05-Mar-15.
  */
-function resetMenuColor(){
-    $("#about-us").css('background-color','');
-    $("#home").css('background-color','');
-    $("#get-cfc").css('background-color','');
-    $("#support").css('background-color','');
-    $(".submenu").hide();
+
+
+var clickedState = 0;
+function resetMenuColor(element){
+
+    console.log(element);
+    if(clickedState!=element) {
+        $(".submenu").hide();
+        $("#about-us").css('background-color', '');
+        $("#home").css('background-color', '');
+        $("#get-cfc").css('background-color', '');
+        $("#support").css('background-color', '');
+
+    }
 }
 var display_type = 'block';
 function aboutUsClicked()
 {
+    clickedState=1;
+    //console.log("~"+1);
     resetMenuColor();
     $("#about-us").css('background-color','rgb(52,152,219)');
     $("#submenu-about-us")
@@ -20,6 +30,8 @@ function aboutUsClicked()
 }
 function getCfcClicked()
 {
+   // console.log("~"+2);
+    clickedState=2;
     resetMenuColor();
     $("#get-cfc").css('background-color','rgb(52,152,219)');
     $("#submenu-get-cfc")
@@ -28,6 +40,8 @@ function getCfcClicked()
 }
 function supportClicked()
 {
+   // console.log("~"+3);
+    clickedState=3;
     resetMenuColor();
     $("#support").css('background-color','rgb(52,152,219)');
     $("#submenu-support")
