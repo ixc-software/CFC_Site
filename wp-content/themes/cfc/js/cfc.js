555c2a4b4d75f597d8572612466690a45e4350cb
betkapolymorphic <a_albert@ukr.net>
2015-03-20 17:56:44 -0300

    category margin top

--- wp-content/themes/cfc/js/cfc.js
+++ wp-content/themes/cfc/js/cfc.js
@@ -264,8 +264,8 @@ $(function(){
 
 		$('.content-left, .content-right').height(innerHeight - 34);
 
-        var topheight =  ($("#main-menu").offset().top+$("#main-menu").height()+$("#submenu-about-us").height());
-        $("#content").css('top',0);
+        var topheight =  $("#submenu-about-us").offset().top+$("#submenu-about-us").height();
+        $("#content").css('top',topheight + 30);
 
 
 		if ($('#homeContent').length > 0 || $('#getAppContent').length > 0)
@@ -743,7 +743,7 @@ $(window).load(function() {
 
 	$("[data-animate]").animate( { height: "show", opacity: "1" } , "500" );
 
-	$('#content').css({ top: $(window).height() }).show().animate({ top: ($("#submenu-about-us").offset().top+$("#submenu-about-us").height()) }, 500);
+	$('#content').css({ top: $(window).height() }).show().animate({ top: ($("#submenu-about-us").offset().top+$("#submenu-about-us").height())+30 }, 500);
 
 	if ($('.category-banner-wrapper').length > 0)
 	{
