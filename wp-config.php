65954ca85b8db060a8159be17e8168d372dcb3f6
Andrew <andrew@cfc.io>
2016-05-03 19:44:07 +0100

    Fixed access to db.

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'cfc_db');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'wordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'GfAggP7L');
 
 /** MySQL hostname */
 define('DB_HOST', '127.0.0.1');
