70e9565d8f2433b08a27ef735480b7ae12d02d8f
Andriy Mykhajlyshyn <amykh2007@gmail.com>
2016-07-04 14:08:43 +0300

    Updated multitel delivery callbacks list.

--- sms-DLR/multitel.php
+++ sms-DLR/multitel.php
@@ -1,12 +1,10 @@
 <?php
 // just add new entries to array
 $urls = array(
-	'http://mac4.ixcglobal.com:8081/multitel.do',
-	'http://mac5.ixcglobal.com:8081/multitel.do',
+	'http://qa1.cfc.io:8081/multitel.do',
 	'http://mac6.ixcglobal.com:8081/multitel.do',
 	'http://mac7.ixcglobal.com:8081/multitel.do',
 	'http://mac8.ixcglobal.com:8081/multitel.do',
-	'http://mac9.ixcglobal.com:8081/multitel.do',
 	);
 $postData = http_build_query($_POST);
 
